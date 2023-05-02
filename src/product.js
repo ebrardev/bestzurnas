48eeaa9354d5ee22dde9aa68145d5249cc812d60
Ebrar <16155052@mersin.edu.tr>
2023-05-02 13:54:05 +0300

    cards styled

--- src/product.js
+++ src/product.js
@@ -1,53 +1,67 @@
 const productData = [
+  {
+      "id": 1,
+      "name": "Koltuk",
+      "city": "İstanbul",
+      "price": 1500,
+      "image": "https://picsum.photos/id/1018/200/300"
+    },
     {
-        "id": 1,
-        "name": "Koltuk",
-        "city": "İstanbul",
-        "price": 1500,
-        "image": "https://picsum.photos/id/1018/200/300"
+      "id": 2,
+      "name": "Masa",
+      "city": "Ankara",
+      "price": 750,
+      "image": "https://picsum.photos/id/1003/200/300"
+    },
+    {
+      "id": 3,
+      "name": "Sandalye",
+      "city": "İzmir",
+      "price": 250,
+      "image": "https://picsum.photos/id/1015/200/300"
+    },
+    {
+      "id": 4,
+      "name": "Komodin",
+      "city": "Bursa",
+      "price": 450,
+      "image": "https://picsum.photos/id/102/200/300"
+    },
+    {
+      "id": 5,
+      "name": "Dolap",
+      "city": "Antalya",
+      "price": 1250,
+      "image": "https://picsum.photos/id/1019/200/300"
       },
       {
-        "id": 2,
-        "name": "Masa",
-        "city": "Ankara",
-        "price": 750,
-        "image": "https://picsum.photos/id/1003/200/300"
+      "id": 6,
+      "name": "Kanepe",
+      "city": "İstanbul",
+      "price": 2500,
+      "image": "https://picsum.photos/id/1018/200/300"
       },
       {
-        "id": 3,
-        "name": "Sandalye",
-        "city": "İzmir",
-        "price": 250,
-        "image": "https://picsum.photos/id/1015/200/300"
-      },
-      {
-        "id": 4,
-        "name": "Komodin",
-        "city": "Bursa",
-        "price": 450,
-        "image": "https://picsum.photos/id/102/200/300"
-      },
-      {
-        "id": 5,
-        "name": "Dolap",
-        "city": "Antalya",
-        "price": 1250,
-        "image": "https://picsum.photos/id/1019/200/300"
-        },
-        {
-        "id": 6,
-        "name": "Kanepe",
-        "city": "İstanbul",
-        "price": 2500,
-        "image": "https://picsum.photos/id/1018/200/300"
-        },
-        {
-        "id": 7,
-        "name": "Yatak",
-        "city": "Ankara",
-        "price": 1750,
-        "image": "https://picsum.photos/id/1003/200/300"
-      }
+      "id": 7,
+      "name": "Yatak",
+      "city": "Ankara",
+      "price": 1750,
+      "image": "https://picsum.photos/id/1003/200/300"
+    },
+    {
+      "id": 8,
+      "name": "Koltuk",
+      "city": "İzmir",
+      "price": 1250,
+      "image": "https://picsum.photos/id/1015/200/300"
+    },
+    {
+      "id": 9,
+      "name": "Masa",
+      "city": "Bursa",
+      "price": 750,
+      "image": "https://picsum.photos/id/102/200/300"
+    },
 ]
 
 export default productData;
