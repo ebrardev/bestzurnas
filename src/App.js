f9ad412c19e0bfb3ea033bed527da632f8d730ef
Ebrar <16155052@mersin.edu.tr>
2023-05-02 12:33:06 +0300

    db.json error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import {useState, useEffect} from 'react';
 
 import './App.css';
+import Card from './components/Card/Card';
 import Navbar from './components/Navbar/Navbar';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
+const App= () =>{
+  const [products,setProducts] = useState([]);
+  useEffect(()=>{
+    axios.get('./db.json').then((response)=>{
+      setProducts(response.data.products);
+  })
+  .catch(error => {
+    console.log('Error fetching products', error);
+  });
+}, [])
 
-    <Navbar/>
-    </div>
-  );
-}
+
+return (
+  <div className="App">
+    <Navbar />
+    {products.map(product => (
+      <Card key={product.id} data={product} />
+    ))}
+  </div>
+);
+};
 
 export default App;
