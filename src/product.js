2c9ab937fe471f459173c2dbc42cc16bdd9456e3
Ebrar <16155052@mersin.edu.tr>
2023-05-03 18:33:12 +0300

    update

--- src/product.js
+++ src/product.js
@@ -4,71 +4,91 @@ const productData = [
       "name": "Mansuroğlu Döner",
       "city": "Mersin",
       "price": 65,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+      
+
     },
     {
       "id": 2,
       "name": "326 Antakya dürüm Esat",
       "city": "Ankara",
       "price": 110,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+     
     },
     {
       "id": 3,
       "name": "By Antakyalı",
       "city": "Istanbul",
       "price": 100,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+   
     },
     {
       "id": 4,
       "name": "Zaka  döner",
       "city": "Izmir",
       "price": 83,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+
     },
     {
       "id": 5,
       "name": "Benim döner",
       "city": "Mersin",
       "price": 72,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+    
       },
       {
       "id": 6,
       "name": "Etibeyaz Döner",
       "city": "Istanbul",
       "price": 110,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+     
+
       },
       {
       "id": 7,
       "name": "Hosta Döner",
       "city": "Ankara",
       "price": 110,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+      
     },
     {
       "id": 8,
       "name": "Lezzy fast food",
       "city": "Izmir",
       "price": 95,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+
     },
     {
       "id": 9,
       "name": "Rey dürüm",
       "city": "Ankara",
       "price": 103,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+   
     },
     {
       "id": 10,
       "name": "Zater döner",
       "city": "Denizli",
       "price": 78,
-      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg"
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+
     },
+    {
+      "id": 11,
+      "name":"Cadde17",
+      "city": "Çanakkale",
+      "price": 60,
+      "image": "https://cdn.discordapp.com/attachments/860256092541157380/1102918514639130684/zurna.jpg",
+
+    }
 ]
 
 export default productData;
